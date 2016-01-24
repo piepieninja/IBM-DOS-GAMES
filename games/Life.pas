64caf381b4e23399a1e2869f8ad4b12671450c42
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-23 23:54:34 -0500

    grid print stable

--- games/Life.pas
+++ games/Life.pas
@@ -12,23 +12,42 @@ uses Crt;
 
 {set some globals so that the program can easily toggle between DOS and OSX}
 var isDOS : boolean = false;
-{set the terminal width and height here}
+   {set the terminal width and height here... but it also has to be ch}
 var grid  : array[1..20, 1..20] of char;
 
-procedure fillGrid(g : array of char)
-var i : 1..size;
-var j : 1..size; 
+procedure fillGrid();
+var i : integer;
+var j : integer;
 begin
-   for i := 1 to size do
+   for i := 1 to length(grid) do
    begin
-      for j := 1 to size do
+      for j := 1 to length(grid) do
       begin
-	 writeln("ayy ");
-      end
-   end
-end
-		      
+	 grid[i][j] := 'X';
+      end;
+   end;
+end;
+
+procedure printGrid();
+var i :  integer;
+var j :  integer;
+begin
+   for i := 1 to length(grid) do
+   begin
+      for j := 1 to length(grid) do
+      begin
+	 {woot print the thing!}
+	 if j = length(grid) then
+	    writeln('', grid[i][j])
+	 else
+	    write('', grid[i][j])
+      end;
+   end;
+end;
+
 begin
    writeln ('Hello, world.');
-   clrscr;
+   fillGrid();
+   printGrid();
+   {clrscr;}
 end.
