439396a0544cd98b1d5e93f9e9da4b4a7ab79f66
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-24 16:08:24 -0500

    begins dynamic array

--- games/Life.pas
+++ games/Life.pas
@@ -11,25 +11,29 @@ program Life;
 uses Crt;
 
 {set some globals so that the program can easily toggle between DOS and OSX}
-var isDOS : boolean = false;
-var cont  : char = 'y';
+var isDOS  : boolean = false;
+var cont   : char = 'y';
 {this will randomly give live to this percentage of cells each step. if 0.0 then it is off and 0.04 would be 4%}
 var randomLife : real = 0.04;
 {set the terminal width and height here... the grid must always be square!}
-var grid  : array[1..60, 1..60] of boolean;
-var temp  : array[1..60, 1..60] of boolean; 
+var width  : integer = 120; {default}
+var height : integer = 40; {default}
+var grid   : array of array of boolean;
+var temp   : array of array of boolean; 
+{var grid : array[1..width, 1..height] of boolean;}
+{var temp : array[1..width, 1..height] of boolean;}
 {set the loop num}
-var x     : integer = 1;
+var x      : integer = 1;
 {the population iteration variable}
-var iter : integer;
+var iter   : integer;
 
 procedure fillGrid();
 var i : integer;
 var j : integer;
 begin
-   for i := 1 to length(grid) do
+   for i := 0 to height - 1 do
    begin
-      for j := 1 to length(grid) do
+      for j := 0 to width - 1 do
       begin
 	 if Random > 0.65 then {make a good number of cells}
 	 begin
@@ -49,12 +53,12 @@ procedure printGrid();
 var i :  integer;
 var j :  integer;
 begin
-   for i := 1 to length(grid) do
+   for i := 0 to height - 1 do
    begin
-      for j := 1 to length(grid) do
+      for j := 0 to width - 1 do
       begin
 	 {woot print the thing!}
-	 if j = length(grid) then
+	 if j = width - 1 then
 	    if grid[i][j] then
 	       writeln('X')
 	    else
@@ -71,7 +75,7 @@ end;
 function countNeighbors(i,j : integer): integer;
 begin
    countNeighbors := 0;
-   if ((i < length(grid) + 1) and (j < length(grid) + 1) and (i > 1) and (j > 1)) then
+   if ((i < height - 1) and (j < width - 1) and (i > 0) and (j > 0)) then
    begin
       if grid[i-1][j-1] then Inc(countNeighbors); 
       if grid[i][j-1] then Inc(countNeighbors);
@@ -92,9 +96,9 @@ var j :   integer;
 var c :   integer;
 begin
    {calculate next step}
-   for i := 1 to length(grid) do
+   for i := 0 to height - 1 do
    begin
-      for j := 1 to length(grid) do
+      for j := 0 to width - 1 do
       begin
 	 c := countNeighbors(i,j);
 	 if c <= 2 then
@@ -116,9 +120,9 @@ begin
       end;
    end;
    {fill from temp grid}
-   for i := 1 to length(grid) do
+   for i := 0 to height - 1 do
    begin
-      for j := 1 to length(grid) do
+      for j := 0 to width - 1 do
       begin
 	 if Random < randomLife then
 	    grid[i][j] := true
@@ -147,7 +151,20 @@ begin
       writeln('What would you like the random population generation percentage to be (in decemal format XXX.XXX). note, that 0.04 is substantial.');
       write('>> ');
       readln(randomLife);
-   
+
+      writeln('Enter the width of the grid. Default is 60');
+      write('>> ');
+      readln(width);
+
+      writeln('Enter the height of the grid. Default is 60');
+      write('>> ');
+      readln(height);
+
+      writeln('array size: ',width*height);
+      
+      setlength(grid,width,height);
+      setlength(temp,width,height);
+      
       Randomize;
       fillGrid();
       while x < iter do {don't do it forever because I can't find out how to sig kill it}
