208a332cb0060ac26432a776b6e7745fd539ad6c
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-25 16:26:07 -0500

    life works on IBM. new fun print.

--- games/Life.pas
+++ games/Life.pas
@@ -1,5 +1,5 @@
-(* 
- Author: Caleb Adams 
+(*
+ Author: Caleb Adams
  Contact: CalebAshmoreAdams@gmail.com
  REPO: https://github.com/piepieninja/IBM-DOS-GAMES
  Discription: This is conway's game of life written in the pacscal programming
@@ -11,29 +11,33 @@ program Life;
 uses Crt;
 
 {set some globals so that the program can easily toggle between DOS and OSX}
-var isDOS  : boolean = false;
-var cont   : char = 'y';
+var isDOS  : boolean;
+var cont   : char;
 {this will randomly give live to this percentage of cells each step. if 0.0 then it is off and 0.04 would be 4%}
-var randomLife : real = 0.04;
+var randomLife : real;
 {set the terminal width and height here... the grid must always be square!}
-var width  : integer = 120; {default}
-var height : integer = 40; {default}
-var grid   : array of array of boolean;
-var temp   : array of array of boolean; 
-{var grid : array[1..width, 1..height] of boolean;}
-{var temp : array[1..width, 1..height] of boolean;}
+var width  : integer; {default}
+var height : integer; {default}
+{var grid   : array of array of boolean;}
+{var temp   : array of array of boolean;}
+var grid : array[1..60, 1..30] of boolean;
+var temp : array[1..60, 1..30] of boolean;
 {set the loop num}
-var x      : integer = 1;
+var x      : integer;
 {the population iteration variable}
 var iter   : integer;
+{for old pascal}
+var passer : integer;
 
-procedure fillGrid();
+procedure fillGrid(var pass : integer);
 var i : integer;
 var j : integer;
 begin
-   for i := 0 to height - 1 do
+   {writeln('entered fill grid');}
+   {writeln('testing random: ', Random);}
+   for i := 1 to height do
    begin
-      for j := 0 to width - 1 do
+      for j := 1 to width do
       begin
 	 if Random > 0.65 then {make a good number of cells}
 	 begin
@@ -49,16 +53,16 @@ begin
    end;
 end;
 
-procedure printGrid();
+procedure printGrid(var pass : integer);
 var i :  integer;
 var j :  integer;
 begin
-   for i := 0 to height - 1 do
+   for i := 1 to height do
    begin
-      for j := 0 to width - 1 do
+      for j := 1 to width do
       begin
 	 {woot print the thing!}
-	 if j = width - 1 then
+	 if j = width then
 	    if grid[i][j] then
 	       writeln('X')
 	    else
@@ -73,32 +77,34 @@ begin
 end;
 
 function countNeighbors(i,j : integer): integer;
+var count : integer;
 begin
-   countNeighbors := 0;
-   if ((i < height - 1) and (j < width - 1) and (i > 0) and (j > 0)) then
+   count := 0;
+   if ((i < height + 1) and (j < width + 1) and (i > 1) and (j > 1)) then
    begin
-      if grid[i-1][j-1] then Inc(countNeighbors); 
-      if grid[i][j-1] then Inc(countNeighbors);
-      if grid[i+1][j-1] then Inc(countNeighbors);
-      if grid[i-1][j] then Inc(countNeighbors);
-      if grid[i][j] then Inc(countNeighbors);
-      if grid[i+1][j] then Inc(countNeighbors);
-      if grid[i-1][j+1] then Inc(countNeighbors);
-      if grid[i][j+1] then Inc(countNeighbors);
-      if grid[i+1][j+1] then Inc(countNeighbors);
-   end
+      if grid[i-1][j-1] then Inc(count);
+      if grid[i][j-1]   then Inc(count);
+      if grid[i+1][j-1] then Inc(count);
+      if grid[i-1][j]   then Inc(count);
+      if grid[i][j]     then Inc(count);
+      if grid[i+1][j]   then Inc(count);
+      if grid[i-1][j+1] then Inc(count);
+      if grid[i][j+1]   then Inc(count);
+      if grid[i+1][j+1] then Inc(count);
+   end;
    {the edge cases could be added}
+   countNeighbors := count;
 end;
 
-procedure step();
+procedure step(var pass : integer);
 var i :   integer;
 var j :   integer;
 var c :   integer;
 begin
    {calculate next step}
-   for i := 0 to height - 1 do
+   for i := 1 to height do
    begin
-      for j := 0 to width - 1 do
+      for j := 1 to width do
       begin
 	 c := countNeighbors(i,j);
 	 if c <= 2 then
@@ -120,9 +126,9 @@ begin
       end;
    end;
    {fill from temp grid}
-   for i := 0 to height - 1 do
+   for i := 1 to height do
    begin
-      for j := 0 to width - 1 do
+      for j := 1 to width do
       begin
 	 if Random < randomLife then
 	    grid[i][j] := true
@@ -130,9 +136,18 @@ begin
 	    grid[i][j] := temp[i][j]
       end
    end;
-end;  
+end;
 
 begin
+   {take care of variable assignments here}
+   isDOS := true;
+   cont := 'y';
+   randomLife := 0.04;
+   width := 72;
+   height := 23;
+   x := 1;
+   passer := 0;
+
    writeln('#==============================#');
    writeln('#  _      _____ ______ ______  #');
    writeln('# | |    |_   _|  ____|  ____| #');
@@ -148,10 +163,11 @@ begin
       write('>> ');
       readln(iter);
 
-      writeln('What would you like the random population generation percentage to be (in decemal format XXX.XXX). note, that 0.04 is substantial.');
+      writeln('What would you like the random population generation percentage');
+      writeln('to be? (in decemal format XXX.XXX). note, that 0.04 is substantial.');
       write('>> ');
       readln(randomLife);
-
+      (*
       writeln('Enter the width of the grid. Default is 60');
       write('>> ');
       readln(width);
@@ -161,28 +177,31 @@ begin
       readln(height);
 
       writeln('array size: ',width*height);
-      
+
       setlength(grid,width,height);
       setlength(temp,width,height);
-      
+      *)
+
       Randomize;
-      fillGrid();
+      fillGrid(passer);
       while x < iter do {don't do it forever because I can't find out how to sig kill it}
       begin
+         clrscr;
 	 x := x + 1;
-	 printGrid();
+	 printGrid(passer);
 	 write('step: ',x);
-	 step();
-	 Delay(100);
-	 clrscr;
+	 step(passer);
+	 {Delay(100);} {the computer prints plenty slow without this}
       end;
 
+      writeln('');
       x := 1; {reset clock}
       write('Life ran ', iter);
       write(' iterations. Do you want to run Life again? (y/n)');
       writeln('');
       write('>> ');
       readln(cont);
-      
+
    end;
 end.
+
